cba3dbd8f8c31fbad5ecbbe062a94ff0b3a49b65
enesozbeks <enesozbekh@gmail.com>
2025-11-05 07:28:18 +0300

    Fix Windows entry point and compiled bindings

--- src/Yesil/Platforms/Windows/Program.cs
+++ src/Yesil/Platforms/Windows/Program.cs
@@ -1,5 +1,4 @@
 using System;
-using Microsoft.UI.Xaml;
 
 namespace Yesil.WinUI;
 
@@ -8,6 +7,7 @@ public static class Program
     [STAThread]
     public static void Main(string[] args)
     {
-        MauiWinUIApplication.Main(args, typeof(App));
+        var app = new App();
+        app.Run(args);
     }
 }
